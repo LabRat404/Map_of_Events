3f88b11b1d3b935ec6110e567550f9c5d4c317ab
doriaaaa_ <doria.tang333@gmail.com>
2022-12-11 03:13:53 +0800

    Revert "add login page"
    This reverts commit 46681d4df6b6964fdcaf435e268b0a2502c33416.

--- 2720_project/src/App.js
+++ 2720_project/src/App.js
@@ -1,27 +1,23 @@
-// import logo from './logo.svg';
-import React, { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
-import Preferences from './components/Preference';
-import Login from './components/Login';
 
 function App() {
-  const [token, setToken] = useState();
-  if(!token) {
-    return <Login setToken={setToken} />
-  }
   return (
-    <div className="wrapper">
-      <h1>Application</h1>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/dashboard" element={<Dashboard />}>
-          </Route>
-          <Route path="/preferences" element={<Preferences />}>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
