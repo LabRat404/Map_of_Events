3d5ed588765343bada47f2890230d209877e0a4d
NTHOscar <72460275+NTHOscar@users.noreply.github.com>
2022-12-16 01:32:15 +0800

    update icons, clickable fav icon

--- 2720_project/src/components/Dashboard.js
+++ 2720_project/src/components/Dashboard.js
@@ -1,7 +1,17 @@
 import React, { Component, useState } from 'react';
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-let venues = ["v1","v2","v3","v4","v5","v6","v7","v8","v9","v10"];
+let venues = [{venuesName: "Tai Po Public Library", quota: 100},
+              {venuesName: "Sha Tin Town Hall (Cultural Activities Hall)", quota: 50},
+              {venuesName: "Fu Shan Public Library", quota: 30},
+              {venuesName: "Hong Kong Cultural Centre (Concert Hall)", quota: 10},
+              {venuesName: "Butterfly Estate Public Library", quota: 70},
+              {venuesName: "Chai Wan Public Library", quota: 80},
+              {venuesName: "City Hall Public Library", quota: 90},
+              {venuesName: "Electric Road Public Library", quota: 50},
+              {venuesName: "Fa Yuen Street Public Library", quota: 20},
+              {venuesName: "Fanling Public Library", quota: 300}
+            ];
 
 function Dashboard(){
   const [sort, updateSort] = useState(false);
@@ -12,7 +22,7 @@ function Dashboard(){
       return "sort";
     }
     else{
-      return venues.map((name,index) => <List name={name} i = {index} key={index}/>);
+      return venues.map((obj,index) => <List venuesObj={obj} i = {index} key={index}/>);
     }
   }
 
@@ -20,8 +30,8 @@ function Dashboard(){
       <>
         <header>        
           <div className="d-flex align-items-center bg-info mb-3">
-            <div id = "userName" className="p-2">User Name</div>
-            <div className="ms-auto p-2"><button type="submit" className="btn btn-primary">Log out</button></div>
+            <div id = "userName" className="p-2"><b>User Name</b></div>
+            <div className="ms-auto p-2"><button type="submit" className="btn btn-primary"><i class="bi bi-person"> </i>Logout</button></div>
           </div>
         </header>
 
@@ -29,11 +39,14 @@ function Dashboard(){
 
           <main>
             <div className="mb-3  d-flex flex-row justify-content-center align-items-center">
-              <div>
+              <div className="p-2">
               <input id = "searchText" type="text" placeholder="Search for locations"></input>
               </div>
               <div className="p-2">
-                <button className="btn btn-primary" type="submit">Search</button>
+                <button className="btn btn-primary" type="submit"><i class="bi bi-search"></i></button>
+              </div>
+              <div className="ms-auto p-2">
+                <button className="btn btn-primary" type="submit">Show favorite locations</button>
               </div>
             </div>
 
@@ -41,9 +54,9 @@ function Dashboard(){
               
               <div id = "table" className="col-5 bg-warning">
                 <div className="d-flex justify-content-between align-items-center bg-secondary">
+                  <div className="p-2"></div>
                   <div className="p-2">Venues</div>
-                  <div className="p-2">Number of Events</div>
-                  <div className="p-2"><button onClick={()=>updateSort(!sort)} type="submit" className="btn btn-primary">Sort</button></div>
+                  <div className="p-2"><button onClick={()=>updateSort(!sort)} type="submit" className="btn btn-primary"><i class="bi bi-sort-down"></i></button></div>
                 </div>
                 <div id = "tableList" className="list-group mb-3">
                   {sortEvent(sort)}
@@ -54,12 +67,13 @@ function Dashboard(){
               <div id = "content" className="col-7 bg-success">
                 test content area(details info/ comment section?)
               </div>
+              
             </div>
           </main>
       
         </div>
         <footer>
-            Last updated on <span id = "lastUpdatedTime">2022-12-14</span>.
+        <i class="bi bi-clock"> </i>Last updated on <span id = "lastUpdatedTime">2022-12-14</span>.
           </footer>
       </>
     );
@@ -70,32 +84,35 @@ function List(props){
   const [selected, updateSelected] = useState(-1);
   const [favorite, updateFavorite] = useState(false);
   let i = props.i; //index
-  let name = props.name;
+  let name = props.venuesObj.venuesName;
+  let quota = props.venuesObj.quota;
   // console.log(name);
   
   // clicked link
   function handleClick(e){
-
+    console.log("selected " + name)
   }
 
   //added to favourite
-  function addFav(e){
+  function addFav(){
     updateFavorite(!favorite);
-    console.log(name + " has been added to favoruite!");
+    // changeFav();
   }
 
   return(
 
     <div className='d-flex flex-row align-items-center'>
-      <div onClick={(e) => handleClick(e)} className="list-group-item list-group-item-action">
-      {/* <a href="#" className="list-group-item list-group-item-action" aria-current="true">
-        {venues[i] + "<br>" + "\nwith quota 2"}
-      </a> */}
-        <div>{name}</div>
-        <div>number of events is: </div>
+      <div onClick={(e) => handleClick(e)} className="list-group-item list-group-item-action p-4 border-bottom border-dark">
+        <div><i class="bi bi-geo-alt"> </i>{name}</div>
+        <div><i class="bi bi-calendar-event"> </i>{quota + " events"}</div>
       </div>
       {/* fav icon star */}
-      <div className='p-2'><button type="submit" className="btn btn-danger" onClick={()=>addFav()}><i class="bi bi-star"></i></button></div>
+      <div onClick={()=>addFav()} className='ms-auto p-2'>
+        <div className=''>
+          {favorite?<i className="bi bi-star-fill"></i>:<i className="bi bi-star"></i>}
+        </div>
+      </div>
+
     </div>
   );
 }
