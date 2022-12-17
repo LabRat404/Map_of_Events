31248a5479e0a94d6995ec451a585ed99778036c
thomplth <29663345+thomplth@users.noreply.github.com>
2022-12-17 11:49:08 +0800

    Created utilities functions for easy access to db

---
+++ 2720_project/src/components/Utilities.js
@@ -0,0 +1,59 @@
+export async function getAllVenues() {
+    return await fetch('http://localhost:3001/getAllVenues', {
+        method: "GET",
+        headers: {
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify({ eventid: 141097 })
+    })
+        .then((res) => res.json())
+        .then((obj) => {
+            console.log(obj);
+        })
+}
+
+export async function getEvent(id) {
+    return await fetch('http://localhost:3001/getEvent', {
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify({ eventid: id })
+    })
+        .then((res) => res.json())
+        .then((obj) => {
+            console.log(obj);
+        })
+}
+
+export async function getCommentList(id) {
+    return await fetch('http://localhost:3001/getComments', {
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify({ venueid: id })
+    })
+        .then((res) => res.json())
+        .then((obj) => {
+            console.log(obj);
+        })
+}
+
+export async function updateComment(id, comment) {
+    return await fetch('http://localhost:3001/updateComment', {
+        method: "PUT",
+        headers: {
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify({
+            venueid: id,
+            comments: [comment],
+        })
+    })
+        .then((res) => res.json())
+        .then((obj) => {
+            if (obj.err)
+                console.log(obj.err);
+        })
+}
